4fbe3c02d195e68461f648f0dd7494b6e77372af
Francisco Alonso <alonso.linux.kde@gmail.com>
2025-12-27 13:44:26 -0300

    Create calculadora_simples.py
    Calculadora simples com operações básicas

---
+++ basico/basico/calculadora_simples.py
@@ -0,0 +1,13 @@
+# Calculadora simples em Python
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+
+print("\nResultados:")
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
